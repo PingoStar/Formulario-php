5c035ca0b8a61d79728540e0f1345592015e850c
PingoStar <pingomoreira@unidavi.edu.br>
2026-03-17 16:22:55 -0300

    Atualização do formulário - versão 2

--- formulario.php
+++ formulario.php
@@ -9,7 +9,7 @@
 
 <form action="destino.php" method="POST">
 
-    <h1>Formulário de Contato</h1>
+    <h1>Formulário de Contato - versão 2</h1>
 
     <label>Nome:</label>
     <input type="text" name="nome" required>
